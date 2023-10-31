233d5e2e16d335a34fdb96af03c226c1b6189138
ktruong09 <149096791+ktruong09@users.noreply.github.com>
2023-10-31 16:00:12 -0500

    Update streamlit_app.py
    fixed indent

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,10 +90,10 @@ def insert_row_snowflake(new_fruit):
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 
 #Add a button to insert fruit into Snowflake
-  if streamlit.button('Add Fruit'):  
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    back_from_function = insert_row_snowflake(add_my_fruit)
-    my_cnx.close()
+if streamlit.button('Add Fruit'):  
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  my_cnx.close()
 
 
 
